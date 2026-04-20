6a781cb98b47d24ea5bd39958fffb8d9be926c28
Scott W. <m@m1is.com>
2026-04-19 20:53:38 -0700

    Add project files.

---
+++ AgentTestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AgentTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, H9k!");
+        }
+    }
+}
